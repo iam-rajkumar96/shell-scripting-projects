eee4685947cf1dc6b9862a3e964ac13e97a7b5a9
iam-rajkumar96 <144085521+iam-rajkumar96@users.noreply.github.com>
2023-09-07 07:15:03 +0530

    s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -10,14 +10,14 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="rajkumar-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="zyz@gmail.com"
+email_address="rajkumarmurugesan28596@gmail.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
-  "Version": "2012-10-17",
+  "Version": "2023-09-07",
   "Statement": [{
     "Action": "sts:AssumeRole",
     "Effect": "Allow",
@@ -104,6 +104,6 @@ aws sns subscribe \
 aws sns publish \
   --topic-arn "$topic_arn" \
   --subject "A new object created in s3 bucket" \
-  --message "Hello from Abhishek.Veeramalla YouTube channel, Learn DevOps Zero to Hero for Free"
+  --message "Hello iam rajkumar"
 
 
